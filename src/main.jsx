39df540b532ecf3293c34f53bea3b4e6aa07124a
dennisie1 <dennis.goosen@citygis.nl>
2026-06-03 22:28:41 +0200

    Update src/main.jsx with relative import

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./crm-app.jsx";
+import App from "./crm-app";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
