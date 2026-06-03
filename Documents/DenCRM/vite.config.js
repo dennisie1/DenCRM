77a3201643a8d63ef8a0ea0cae2c677b6842c82a
Dennisie1 <dennis.goosen@citygis.nl>
2026-06-03 21:23:09 +0200

    Update Wed-06-03 2026:21

---
+++ Documents/DenCRM/vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // Pas 'base' aan naar jouw GitHub repository-naam als je GitHub Pages gebruikt
+  // Voorbeeld: base: "/dencrm/"
+  base: "/dencrm/",
+});
